edee10b00185a31200c64e8ca30147c70e7df1f9
moussasg <117439847+moussasg@users.noreply.github.com>
2023-03-16 21:24:22 +0100

    yy

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+  return (
+   <>
+   <h1> Ma position Hydra </h1>
+   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25578.110244640386!2d3.007740294686316!3d36.74023905831581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128fb20a92cfe02b%3A0x5417f83e218b9393!2sHydra!5e0!3m2!1sfr!2sdz!4v1678997642374!5m2!1sfr!2sdz" 
+   width="600" height="200" style={{ border: "0" }}
+ allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  
+   </>
+  )
+}
+
+export default App
